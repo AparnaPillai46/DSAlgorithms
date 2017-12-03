19d234bc414142c5369e9f038ff9ea116f6dd6d0
Aparna Pillai <pillaiaparna@gmail.com>
2017-12-02 22:31:19 -0800

    Added Exceptions

--- DSAlgorithms/src/sorting/QuickSortNutsAndBolts.java
+++ DSAlgorithms/src/sorting/QuickSortNutsAndBolts.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class QuickSortNutsAndBolts {
 
-	private static int[] nut = { 64, 35, 36, 37, 38, 94, 76, 56, 45 };
+	private static int[] nut = { 64, 35, 36, 37, 38, 94, 76, 56, 45, 90, 67, 89, 78 };
 
-	private static int[] comp = { 36, 37, 35, 38, 64, 94, 76, 56, 45 };
+	private static int[] comp = { 36, 37, 35, 38, 64, 94, 76, 56, 45, 78, 89, 67, 90 };
 
 	public static void main(String[] args) {
 
@@ -17,16 +17,25 @@ public class QuickSortNutsAndBolts {
 
 	}
 
-	public static void quickSort(int[] nut, int[] comp, int start, int end) {
-
+	public static void quickSort(int[] nut, int[] bolt, int start, int end) {
+		  if (nut == null || nut == null ) {
+		      throw new IllegalArgumentException("invalid input ");
+		  }
+		  if (nut.length != bolt.length ) {
+		      throw new IllegalArgumentException(" nut and bolt length should be same  ");
+		  }
+		  
 		if (start > end)
 			return;
 
-		int pivot = partition(nut, comp[end], start, end);
-		pivot = partition(comp, nut[pivot], start, end);
-
-		quickSort(nut, comp, start, pivot - 1);
-		quickSort(nut, comp, pivot + 1, end);
+		int pivot = partition(nut, bolt[end], start, end);
+		
+		pivot = partition(bolt, nut[pivot], start, end);
+		  if (nut[pivot] != bolt[pivot]) {
+		      throw new IllegalArgumentException("nut and bolt dont match");
+		    }
+		quickSort(nut, bolt, start, pivot - 1);
+		quickSort(nut, bolt, pivot + 1, end);
 
 	}
 
